09ad41e3c08187b2669bbe953adf99b0f5de1c83
Lithry <facundosargiotti@gmail.com>
2017-04-15 22:32:01 -0300

    Player Factory, Builder y Manager.

---
+++ MovileGame/Assets/Scripts/Factory/PlayerFactory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFactory : MonoBehaviour {
+    static public PlayerFactory instance = null;
+    public GameObject playerPrefab;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+
+    public GameObject Create(string type)
+    {
+        switch (type)
+        {
+            case "Player":
+                GameObject player = Instantiate(playerPrefab, new Vector3(0, 0.5f, 0), new Quaternion(0, 0, 0, 1));
+                return player;
+            default:
+                return null;
+        }
+    }
+
+    public void Recycle(GameObject obj)
+    {
+        Destroy(obj);
+    }
+}
