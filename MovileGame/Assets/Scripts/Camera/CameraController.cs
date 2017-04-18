e09a6f571125bbfd498421e73c56ef82d55bb8c3
Lithry <facundosargiotti@gmail.com>
2017-04-18 00:23:22 -0300

    - Respawn en radio al player. - Camara sigue al player.

---
+++ MovileGame/Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(PlayerManager.instance.PlayerPos().x, PlayerManager.instance.PlayerPos().y + 3, PlayerManager.instance.PlayerPos().z);	
+	}
+}
--- MovileGame/Assets/Scripts/Manager/ZombieManager.cs
+++ MovileGame/Assets/Scripts/Manager/ZombieManager.cs
@@ -6,27 +6,34 @@ public class ZombieManager : MonoBehaviour {
     static public ZombieManager instance = null;
     private GameObject player;
 
-    public float respawnDelay;
+    private float respawnDelay;
     private float respawn;
 
     // Use this for initialization
     void Start () {
         instance = this;
         player = GameObject.FindGameObjectWithTag("Player");
-        respawnDelay = 5;
+        respawnDelay = 0;
     }
 	
 	// Update is called once per frame
 	void Update () {
+        Respawn();        
+	}
+
+    private void Respawn() {
         respawn += Time.deltaTime;
 
         if (respawn > respawnDelay)
         {
-            ZombieBuilder.instance.Build("Zombie", new Vector3(0, 0.2f, 0), new Vector3(0, 0, 0));
+            int angle = (int)Random.Range(1.00f, 360.99f);
+            Vector3 respawnPos = new Vector3(Mathf.Cos(angle) * 3, 0.3f, Mathf.Sin(angle) * 3);
+
+            ZombieBuilder.instance.Build("Zombie", respawnPos + PlayerPos(), new Vector3(0, 0, 0));
             respawn = 0;
-            respawnDelay = Random.Range(0.5f, 4.0f);
+            respawnDelay = Random.Range(0.5f, 2.0f);
         }
-	}
+    }
 
     public Vector3 PlayerPos() {
         return player.transform.position;
